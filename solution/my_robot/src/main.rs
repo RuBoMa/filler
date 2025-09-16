1c22a16b47dd56562306ed361d8cfacd6fafb79a
Roope <roope.hongisto@gritlab.ax>
2025-09-16 15:47:45 +0300

    trying to read input and get some output

--- solution/my_robot/src/main.rs
+++ solution/my_robot/src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let first_line = lines.next().unwrap().unwrap();
+    let (player_char, last_piece_char) = if first_line.contains("p2") {
+        ('$','s')
+    } else {
+        ('@','a')
+    };
+    println!("Player chars: {} {}", player_char, last_piece_char);
+
+    let anfield_info = lines.next().unwrap().unwrap();
+    let parts: Vec<&str> = anfield_info.split_whitespace().collect();
+    let rows: usize = parts[1].parse().unwrap();
+    let cols: usize = parts[2].trim_end_matches(':').parse().unwrap();
+    println!("Anfield size: {} rows x {} cols", rows, cols);
+
+    let mut anfield: Vec<Vec<char>> = Vec::new();
+    for _ in 0..rows {
+        let line = lines.next().unwrap().unwrap();
+        let row_data: Vec<char> = line[4..].chars().collect();
+        anfield.push(row_data);
+    }
+
+    for row in &anfield {
+        println!("{}", row.iter().collect::<String>());
+    }
+
+    println!("0 0");
 }
