f10a9804d05b2625e77563b74c1047ccfd876c45
ylee <leeyn.shun@gmail.com>
2025-09-17 19:07:58 +0300

    delete target folder

--- solution/my_robot/target/release/deps/my_robot-2f5af059157cbe47.d
+++
@@ -1,5 +0,0 @@
-/Users/roope.hongisto/Desktop/filler/solution/my_robot/target/release/deps/my_robot-2f5af059157cbe47.d: src/main.rs
-
-/Users/roope.hongisto/Desktop/filler/solution/my_robot/target/release/deps/my_robot-2f5af059157cbe47: src/main.rs
-
-src/main.rs:
